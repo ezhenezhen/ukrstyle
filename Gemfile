6ba91ca043b2714232316580c68327061e7e5dec
Eugene Karpenko <ezhenezhen@gmail.com>
2012-12-17 16:19:19 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+gem 'paperclip'
+gem 'devise', '~> 2.1.2'
+gem 'typus'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ukrstyle::Application.config.secret_token = '95a125ef3ea75cf361d8ee5e80f3a4be6f6a7c3058c066f5f6355d75c0a2abcc29c4bf733d2145035fac085cf277af0e935fce8f05da5359b9f8faf588fa4d88'
