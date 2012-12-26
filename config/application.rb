a1194710f076d216550c177b1447a752449b60cc
Eugene Karpenko <ezhenezhen@gmail.com>
2012-12-26 14:58:19 +0200

    config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -53,8 +53,11 @@ module Ukrstyle
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
